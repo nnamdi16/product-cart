1cbbd33fa437efabf1d5276a6e9d1742d244613e
Nnamdi <nnwabuokei@paga.com>
2021-01-05 12:00:54 +0100

    implemented addToCart method on the service layer

--- api/cart/cart.dao.js
+++ api/cart/cart.dao.js
@@ -5,19 +5,44 @@
 
 const { model } = require("mongoose");
 const cartSchema = require("./cart.model");
+const {getProductById} = require('../products/products.dao');
+const {getPriceById} = require('../price/price.dao');
 
 cartSchema.statics = {
-  addToCart: (data, cb) => {
-    const cart = new CartModel(data);
-    cart.save(cb);
+  
+  addToCart: async (data, cb) => {
+   try {
+    const { productId, quantity }  = data;
+    const product = await getProductById(productId);
+    if (product) {
+      const {quantityInStock,priceId} = product;
+      const priceDetails = await getPriceById(priceId);
+      const {amount} = priceDetails;
+      console.log(amount);
+      const quantityAvailable = quantityInStock - quantity;
+      console.log(quantityAvailable);
+      const price = amount * quantity
+      const cart = new CartModel(data);
+      cart["price"] = price;
+      return await cart.save(cb);
+    }
+
+    return {
+      error: true
+    }
+   } catch (error) {
+     throw new Error(error);
+   }
+   
   },
+
   getCart: async (query, cb) => {
     await CartModel.find(query, cb);
   },
   // getByName: async (query, cb) => {
   //   await PriceModel.findOne(query, cb);
   // },
-  updateCart: async function(query, updateData, cb) {
+  updateCart: async function(query, updateData, state, cb) {
     await CartModel.findOneAndUpdate(
       query,
       { $set: updateData },
