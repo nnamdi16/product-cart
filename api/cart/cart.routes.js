68df9356ffee24004885df8ed51b51912eb7f75a
Nnamdi <nnwabuokei@paga.com>
2021-01-05 12:01:38 +0100

    updated and added the put route

--- api/cart/cart.routes.js
+++ api/cart/cart.routes.js
@@ -1,17 +1,17 @@
 const {
   addToCart,
-  getHeros,
-  getHero,
-  updateHero,
-  removeHero
+  getCart,
+  // getHero,
+  updateCart,
+  // removeHero
 } = require("./cart.controller");
 const express = require("express");
 const router = express.Router();
 
 router.route("/create").post(addToCart);
-// router.route("/get").get(getHeros);
+// router.route("/get").get(getCart);
 // router.route("/get/:name").get(getHero);
-// router.put("/update/:id", updateHero);
+router.put("/update/:id", updateCart);
 // router.delete("/remove/:id", removeHero);
 
 module.exports = router;
